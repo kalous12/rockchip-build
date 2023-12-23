da2eace70ab8dde6269a6c130f0570d6a624d6eb
kalous12 <2797795537@qq.com>
2023-12-23 02:50:42 +0000

    更新uboot启动，自动加载设备树

---
+++ packages/rootfs/lubancat.cmd
@@ -0,0 +1,137 @@
+# This is a boot script for U-Boot
+#
+# Recompile with:
+# mkimage -A arm64 -O linux -T script -C none -n "Boot Script" -d lubancat.cmd lubancat.scr
+
+# get adc channel 2
+adc_read 2
+
+# get adc channel 3
+adc_read 3
+
+echo "adc channel 2 index is ${adc_index_2}"
+echo "adc channel 3 index is ${adc_index_3}"
+
+if test "${board}" = "evb_rk3588" ;then
+    if test "${adc_index_2}" = "1" && test "${adc_index_3}" = "1" ;then
+        echo "board name is lubancat4"
+        echo "device tree is rk3588s-lubancat-4.dtb"
+        setenv board_name "lubancat-4"
+        setenv board_dtb "rk3588s-lubancat-4.dtb"
+    else 
+        echo "board is not lubancat"
+        setenv is_not_lubancat "yes"
+    fi
+
+elif test "${board}" = "evb_rk3568" ;then
+    if test "${adc_index_2}" = "0" && test "${adc_index_3}" = "0" ;then
+        echo "board name is lubancat1"
+        echo "device tree is rk3566-lubancat-1.dtb"
+        setenv board_name "lubancat-1"
+        setenv board_dtb "rk3566-lubancat-1.dtb"
+
+    elif test "${adc_index_2}" = "0" && test "${adc_index_3}" = "1" ;then
+        echo "board name is lubancat1f"
+        echo "device tree is rk3566-lubancat-1io.dtb"
+        setenv board_name "lubancat-1f"
+        setenv board_dtb "rk3566-lubancat-1io.dtb"
+
+    elif test "${adc_index_2}" = "0" && test "${adc_index_3}" = "2" ;then
+        echo "board name is lubancat1b"
+        echo "device tree is rk3566-lubancat-1io.dtb"
+        setenv board_name "lubancat-1b"
+        setenv board_dtb "rk3566-lubancat-1io.dtb"
+
+    elif test "${adc_index_2}" = "1" && test "${adc_index_3}" = "0" ;then
+        echo "board name is lubancat1n"
+        echo "device tree is rk3566-lubancat-1n.dtb"
+        setenv board_name "lubancat-1n"
+        setenv board_dtb "rk3566-lubancat-1n.dtb"
+
+    elif test "${adc_index_2}" = "1" && test "${adc_index_3}" = "2" ;then
+        echo "board name is lubancat2bi"
+        echo "device tree is rk3568-lubancat-2io.dtb"
+        setenv board_name "lubancat-2bi"
+        setenv board_dtb "rk3568-lubancat-2io.dtb"
+
+    elif test "${adc_index_2}" = "2" && test "${adc_index_3}" = "0" ;then
+        echo "board name is lubancat0n"
+        echo "device tree is rk3566-lubancat-0.dtb"
+        setenv board_name "lubancat-0n"
+        setenv board_dtb "rk3566-lubancat-0.dtb"
+
+    elif test "${adc_index_2}" = "2" && test "${adc_index_3}" = "1" ;then
+        echo "board name is lubancat1h"
+        echo "device tree is rk3566-lubancat-1h.dtb"
+        setenv board_name "lubancat-1h"
+        setenv board_dtb "rk3566-lubancat-1h.dtb"
+
+    elif test "${adc_index_2}" = "3" && test "${adc_index_3}" = "0" ;then
+        echo "board name is lubancat0w"
+        echo "device tree is rk3566-lubancat-0.dtb"
+        setenv board_name "lubancat-0w"
+        setenv board_dtb "rk3566-lubancat-0.dtb"
+
+    elif test "${adc_index_2}" = "4" && test "${adc_index_3}" = "0" ;then
+        echo "board name is lubancat2"
+        echo "device tree is rk3568-lubancat-2.dtb"
+        setenv board_name "lubancat-2"
+        setenv board_dtb "rk3568-lubancat-2.dtb"
+
+    elif test "${adc_index_2}" = "4" && test "${adc_index_3}" = "2" ;then
+        echo "board name is lubancat2-v1"
+        echo "device tree is rk3568-lubancat-2-v1.dtb"
+        setenv board_name "lubancat-2-v1"
+        setenv board_dtb "rk3568-lubancat-2-v1.dtb"
+
+    elif test "${adc_index_2}" = "4" && test "${adc_index_3}" = "3" ;then
+        echo "board name is lubancat2-v2"
+        echo "device tree is rk3568-lubancat-2-v2.dtb"
+        setenv board_name "lubancat2-v2"
+        setenv board_dtb "rk3568-lubancat-2-v2.dtb"
+
+    elif test "${adc_index_2}" = "5" && test "${adc_index_3}" = "0" ;then
+        echo "board name is lubancat2n"
+        echo "device tree is rk3568-lubancat-2n.dtb"
+        setenv board_name "lubancat-2n"
+        setenv board_dtb "rk3568-lubancat-2n.dtb"
+
+    elif test "${adc_index_2}" = "6" && test "${adc_index_3}" = "0" ;then
+        echo "board name is lubancat2n"
+        echo "device tree is rk3568-lubancat-2n.dtb"
+        setenv board_name "lubancat-2n"
+        setenv board_dtb "rk3568-lubancat-2n.dtb"
+
+    elif test "${adc_index_2}" = "5" && test "${adc_index_3}" = "1" ;then
+        echo "board name is lubancat2n-v1"
+        echo "device tree is rk3568-lubancat-2n-v1.dtb"
+        setenv board_name "lubancat-2n-v1"
+        setenv board_dtb "rk3568-lubancat-2n-v1.dtb"
+
+    elif test "${adc_index_2}" = "6" && test "${adc_index_3}" = "1" ;then
+        echo "board name is lubancat2h"
+        echo "device tree is rk3568-lubancat-2h.dtb"
+        setenv board_name "lubancat-2h"
+        setenv board_dtb "rk3568-lubancat-2h.dtb"
+
+    elif test "${adc_index_2}" = "7" && test "${adc_index_3}" = "0" ;then
+        echo "board name is lubancat2f"
+        echo "device tree is rk3568-lubancat-2io.dtb"
+        setenv board_name "lubancat-2f"
+        setenv board_dtb "rk3568-lubancat-2io.dtb"
+
+    elif test "${adc_index_2}" = "7" && test "${adc_index_3}" = "1" ;then
+        echo "board name is lubancat2b"
+        echo "device tree is rk3568-lubancat-io.dtb"
+        setenv board_name "lubancat2b"
+        setenv board_dtb "rk3568-lubancat-io.dtb"
+    else 
+        echo "board is not lubancat"
+        setenv is_not_lubancat "yes"
+    fi
+else
+    echo "board is not lubancat"
+    setenv is_not_lubancat "yes"
+fi
+
+
--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -110,55 +110,11 @@ tar -xpf "${rootfs}" -C ${rootfs_dir}
 # [ -z "${img##*desktop*}" ] && bootargs="quiet splash plymouth.ignore-serial-consoles" || bootargs=""
 
 # Uboot script
-cat > ${boot_dir}/boot.cmd << 'EOF'
-# This is a boot script for U-Boot
-#
-# Recompile with:
-# mkimage -A arm64 -O linux -T script -C none -n "Boot Script" -d boot.cmd boot.scr
-
-setenv load_addr "0x7900000"
-setenv overlay_error "false"
-
-echo "Boot script loaded from ${devtype} ${devnum}"
-echo "start to go"
-
-if test -e ${devtype} ${devnum}:${distro_bootpart} /uEnv.txt; then
-    echo "load ${devtype} ${devnum}:${distro_bootpart} ${load_addr} /uEnv.txt"
-	load ${devtype} ${devnum}:${distro_bootpart} ${load_addr} /uEnv.txt
-	env import -t ${load_addr} ${filesize}
-fi
-
-echo "load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr_r} /dtbs/${fdtfile}"
-load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr_r} /dtbs/${fdtfile}
-fdt addr ${fdt_addr_r} && fdt resize 0x10000
-
-for overlay_file in ${overlays}; do
-    if load ${devtype} ${devnum}:${distro_bootpart} ${fdtoverlay_addr_r} /dtbs/overlays/${overlay_prefix}-${overlay_file}.dtbo; then
-        echo "Applying device tree overlay: /dtbs/overlays/${overlay_prefix}-${overlay_file}.dtbo"
-        fdt apply ${fdtoverlay_addr_r} || setenv overlay_error "true"
-    elif load ${devtype} ${devnum}:${distro_bootpart} ${fdtoverlay_addr_r} /dtbs/overlays/${overlay_file}.dtbo; then
-        echo "Applying device tree overlay: /dtbs/overlays/${overlay_file}.dtbo"
-        fdt apply ${fdtoverlay_addr_r} || setenv overlay_error "true"
-    elif load ${devtype} ${devnum}:${distro_bootpart} ${fdtoverlay_addr_r} /dtbs/overlays/rk3588-${overlay_file}.dtbo; then
-        echo "Applying device tree overlay: /dtbs/overlays/rk3588-${overlay_file}.dtbo"
-        fdt apply ${fdtoverlay_addr_r} || setenv overlay_error "true"
-    fi
-done
-if test "${overlay_error}" = "true"; then
-    echo "Error applying device tree overlays, restoring original device tree"
-    load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr_r} /dtbs/${fdtfile}
-fi
-
-echo "load ${devtype} ${devnum}:${distro_bootpart} ${kernel_addr_r} /vmlinuz-${uname_r}"
-load ${devtype} ${devnum}:${distro_bootpart} ${kernel_addr_r} /vmlinuz-${uname_r}
-
-echo "load ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} /initrd.img-${uname_r}"
-load ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} /initrd.img-${uname_r}
-
-echo "booti ${kernel_addr_r} ${ramdisk_addr_r}:${filesize} ${fdt_addr_r}"
-booti ${kernel_addr_r} ${ramdisk_addr_r}:${filesize} ${fdt_addr_r}
-EOF
+cp ../package/rootfs/boot.cmd ${boot_dir}/
 mkimage -A arm64 -O linux -T script -C none -n "Boot Script" -d ${boot_dir}/boot.cmd ${boot_dir}/boot.scr
+
+cp ../package/rootfs/lubancat.cmd ${boot_dir}/
+mkimage -A arm64 -O linux -T script -C none -n "Boot Script" -d ${boot_dir}/lubancat.cmd ${boot_dir}/lubancat.scr
 
 # Uboot env
 cat > ${boot_dir}/uEnv.txt << EOF
